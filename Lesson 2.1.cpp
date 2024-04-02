e39b1f1e2cb557277d76f31c8b4e29df002a5f89
hp victus <hp victus@LAPTOP-2I0QRUOT>
2024-04-02 14:14:23 +0300

    Добавьте файлы проекта.

---
+++ Lesson 2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int priseProduct = 2048;
+    int delivery = 512;
+    int sale = 1024;
+    int priseTotal = priseProduct + delivery - sale;
+
+
+    cout << "Prise: " << priseProduct << "\n";
+    cout << "Delivery: " << delivery << "\n";
+    cout << "Sale: " << sale << "\n";
+    cout << "Total prise: " << priseTotal << "\n";
+
+    return 0;
+}
+
